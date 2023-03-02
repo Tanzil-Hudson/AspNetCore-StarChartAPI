ded910193af05d17cab36d0f27fd3ac20cc3127d
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-03-02 13:56:36 -0500

    test12

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace StarChart
 {
-    public class Startup
+    public class Startup : DbContext
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
