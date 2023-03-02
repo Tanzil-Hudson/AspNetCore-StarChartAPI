cdec3a0805ecbe95391579f0e0ed2bf425e418c9
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-03-02 14:53:40 -0500

    stash2

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+    }
+
+
+}
