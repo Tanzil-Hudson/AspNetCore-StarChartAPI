39b4e3a7d007aac17a3f75d3fe72f2ef1d07a635
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-03-02 18:27:46 -0500

    FINALLY

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -81,6 +81,31 @@ namespace StarChart.Controllers
             return NoContent();
         }
 
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name) 
+        {
+            var existingObject = _context.CelestialObjects.Find(id);
+            if (existingObject == null)
+                return NotFound();
+            existingObject.Name = name;
+            _context.CelestialObjects.Update(existingObject);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
+            if (!celestialObjects.Any())
+                return NotFound();
+            _context.CelestialObjects.RemoveRange(celestialObjects);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
+
 
     }
 
